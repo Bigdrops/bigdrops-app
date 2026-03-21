8e7a67ffb7e82c477589361feafffaf6dbac83c6
Bigdrops <92942157+Bigdrops@users.noreply.github.com>
2026-03-21 17:33:59 +0100

    Update add_po_number_to_quotations.sql
    trick

--- sql/add_po_number_to_quotations.sql
+++ sql/add_po_number_to_quotations.sql
@@ -2,3 +2,5 @@ alter table public.quotations
 add column if not exists po_number text;
 
 comment on column public.quotations.po_number is 'Optional purchase order number associated with the quotation.';
+
+the wor is done 
