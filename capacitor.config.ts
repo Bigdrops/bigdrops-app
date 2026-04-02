917e48666c52c2df7d123263588598f1e222db25
Bigdrops <jaiyewisdom@gmail.com>
2026-04-02 20:46:08 +0100

    chore: set up Android project for APK builds

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bigdrops.app',
+  appName: 'BigDrops',
+  webDir: 'dist'
+};
+
+export default config;
